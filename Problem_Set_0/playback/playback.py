d2edbfb00799e108a197bc13230b6b4c0b74d430
Lauren <lauren.r.s.steenkamp@gmail.com>
2025-08-15 10:22:01 +0200

    implemented a function in playback.py

--- Problem_Set_0/playback/playback.py
+++ Problem_Set_0/playback/playback.py
@@ -1,5 +1,6 @@
 #in this activity I will make use of the replace() function to replace whitespace with three dots
+def main():
+    playback = input("Can You say that again please? : ")
+    print(playback.replace(" ","..."))
 
-playback = input("Can You say that again please? : ")
-
-print(playback.replace(" ","..."))
+main()
